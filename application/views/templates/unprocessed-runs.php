4d94ff51dce992db25a6d79faac7c78ddcf4fb36
Kevin Ford <kefo@3windmills.com>
2013-07-04 23:59:21 -0400

    New template for unprocessed runs.

--- application/views/templates/unprocessed-runs.php
+++ application/views/templates/unprocessed-runs.php
@@ -1,29 +1,43 @@
-	<h1>Runs</h1>
-		<table cellpadding="0" cellspacing="0" border="0" class="tablesorter table table-striped table-bordered" id="runsTable">
-			<thead> 
-				<tr> 
-					<th>Date</th>
-					<th>City</th>
-					<th>State</th>
-					<th>Country</th>
-					<th>Time</th>
-					<th>Distance (miles)</th>
-					<th>Pace</th>
-				</tr>
-			<thead> 
-			<tbody> 
-			<?php 
-			foreach ($runs as $r) {
-				echo "<tr>\n";
-				echo "\t<td><a href=\"" . $r["relativeuri"] . "/\">" . $r["date"] . "</a></td>\n";
-				echo "\t<td>" . $r["city"] . "</td>\n";
-				echo "\t<td>" . $r["statename"] . "</td>\n";
-				echo "\t<td>" . $r["country"] . "</td>\n";
-				echo "\t<td>" . $r["runtime"] . "</td>\n";
-				echo "\t<td>" . $r["miles"] . "</td>\n";
-				echo "\t<td>" . $r["pacetime"] . "</td>\n";
-				echo "</tr>\n";
-			}
-			?>
-			</tbody>
-		</table>
+        <script>
+            function loadrun(rowid){
+                var rowid = rowid;
+                $('#'+rowid + ' td').css('background-color', '#fcf8e3');               
+                $.ajax({
+                    type: "GET",
+                    url: "<?php echo RELATIVEPATH;?>run/" + rowid + "/load",
+                    success: function(html){
+                        //$('#'+rowid + ' tr').addClass("alert alert-success");
+                        $('#'+rowid + ' td').css('background-color', '#dff0d8');
+                        setTimeout(
+                            function() 
+                            {
+                                $('#'+rowid).remove();
+                            }, 2000);
+                    }
+                });                 
+            }
+        </script>
+        <h1>Runs</h1>
+        <table cellpadding="0" cellspacing="0" border="0" class="tablesorter table table-striped table-bordered" id="runsTable">
+            <thead> 
+                <tr> 
+                    <th>ID</th>
+                    <th>Filesize</th>
+                    <th>Filename</th>
+                    <th> </th>
+		</tr>
+            <thead> 
+            <tbody> 
+            <?php 
+                foreach ($xmlfiles as $r) {
+                    echo "<tr id=\"" . $r["id"] . "\">\n";
+                    echo "\t<td>" . $r["id"] . "</td>\n";
+                    echo "\t<td>" . $r["filesize"] . "</td>\n";
+                    echo "\t<td>" . $r["filename"] . "</td>\n";
+                    //echo "\t<td><a href=\"" . RELATIVEPATH . "run/" . $r["id"] . "/load\">Load</a></td>\n";
+                    echo "\t<td><a href=\"javascript:void(0);\" onclick=\"javascript:loadrun('" . $r["id"] . "');\">Load</a></td>\n";
+                    echo "</tr>\n";
+		}
+            ?>
+            </tbody>
+	</table>
